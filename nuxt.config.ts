cd59d3d1a2e1f078ff4b6543355721ee00f65a67
Riva Almero <riva@dev.netkromsolution.com>
2023-09-25 13:23:46 +0700

    chore: configure packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: '',
+      apiDokterUrl: '',
+      parseUrl: '',
+      faskesId: '',
+      clientDomain: ''
+    }
+  },
+
+  modules: [
+    '@pinia/nuxt'
+  ],
+
+  css: ['@/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    }
+  }
 })
