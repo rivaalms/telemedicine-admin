e05af4ce8273d4efe5b11beef06b1a2bb0354ac4
Riva Almero <riva@dev.netkromsolution.com>
2023-09-26 12:26:34 +0700

    feat: homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-   index
+   <u-card>
+      <p class="text-lg font-semibold">
+         Selamat datang, {{ user.name || '' }}!
+      </p>
+   </u-card>
 </template>
+
+<script setup lang="ts">
+const store = useAppStore()
+const authStore = useAuthStore()
+const user = computed(() => authStore.getUser)
+
+store.title = 'Beranda'
+useHead({ title: store.getTitle })
+</script>
