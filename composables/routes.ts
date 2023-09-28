0e2dbeb677e59a58bd84e9565dfa9893232169fb
rivaalms <rvalms@icloud.com>
2023-09-28 22:32:34 +0700

    fix(sidebar): sidebar routes UI

---
+++ composables/routes.ts
@@ -0,0 +1,22 @@
+export const useRoutes = [
+   {
+      label: 'Beranda',
+      to: '/',
+      // icon: 'i-heroicons-home',
+   },
+   {
+      label: 'Emergency',
+      to: '/emergency',
+      // icon: 'i-heroicons-exclamation-triangle'
+   },
+   {
+      label: 'Users',
+      // icon: 'i-heroicons-users',
+      children: [
+         {
+            label: 'Dokter',
+            to: '/users/doctors/list',
+         }
+      ]
+   }
+]
