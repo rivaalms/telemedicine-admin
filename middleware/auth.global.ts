88d8a03143e64a5b17af1055b083ba043aa2106a
rivaalms <rvalms@icloud.com>
2023-09-30 12:05:26 +0700

    fix: update middleware
    delete ofetch configuration on middleware, add global route middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,31 +1,5 @@
-import { ofetch } from 'ofetch'
-
 export default defineNuxtRouteMiddleware(async (to, from) => {
    const authStore = useAuthStore()
-   const config = useRuntimeConfig()
-
-   if (to.path !== '/login' && authStore.isLoggedIn) {
-      globalThis.$fetch = ofetch.create({
-         baseURL: `${config.public.apiBaseUrl}`,
-
-         onRequest({ options, error }: any) {
-            if (authStore.isLoggedIn) {
-               options.headers = {
-                  'Authorization': `Bearer ${authStore.getUser['AUTH-TOKEN']}`,
-                  'Accept': 'application/json'
-               }
-            }
-
-            if (error) throw error
-         },
-
-         onResponseError({ response }: any) {
-            if (response.status == 401) {
-               authStore.$reset()
-               if (localStorage.getItem('user')) localStorage.removeItem('user')
-               navigateTo('/login')
-            }
-         }
-      })
-   }
+   if (to.path !== '/login' && !authStore.isLoggedIn) navigateTo('/login')
+   else if (to.path === '/login' && authStore.isLoggedIn) navigateTo('/')
 })
