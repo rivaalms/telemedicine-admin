23ad2cfaade9abfea7c0cc7fd39bef15d4016376
rivaalms <rvalms@icloud.com>
2023-10-16 20:04:55 +0700

    refactor: type definition for broadcast message response

--- utils/api/broadcast.ts
+++ utils/api/broadcast.ts
@@ -1,9 +1,9 @@
-export async function broadcast (image: any, data: Model.Broadcast) : Promise <any> {
+export async function broadcast (image: any, data: Model.Broadcast) : Promise <string> {
    const payload = new FormData()
    payload.append('image', image)
    payload.append('data', JSON.stringify(data))
 
-   const response = await $fetch <API.Response <any>> (`/broadcast`, {
+   const response = await $fetch <API.Response <string>> (`/broadcast`, {
       method: 'POST',
       body: payload
    })
