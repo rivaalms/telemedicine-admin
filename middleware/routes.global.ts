7ca4bd238043f1ebaedf793f592cb72e417e9228
Riva Almero <riva@dev.netkromsolution.com>
2023-10-26 11:50:04 +0700

    perf(middleware): optimize routes middleware

--- middleware/routes.global.ts
+++ middleware/routes.global.ts
@@ -1,18 +1,14 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-   const excludedRoutes = [
-      'login',
-      'forgot-password',
-      'error-code',
-      'profile',
-      'users-doctors-str'
-   ]
+   const route = useRouter().getRoutes().find(item => item.name === to.name)
+   if (route) {
+      let roledRoutes = useRoutes.find(item => item.to === route.path || item.children?.find(child => child.to === route.path))
 
-   if (!excludedRoutes.includes(to.name as string)) {
-      let route = useRoutes.find((route) => route.to === to.path || route.children?.find(child => child.to === to.path))
-      if (route?.children) route = route.children.find((child) => child.to === to.path)
-
-      if (!route) return navigateTo('/error/404', { replace: true })
-      const role = useAuthStore().getRole
-      if (route.roles !== '*' && !route?.roles?.includes(role)) return navigateTo('/error/403', { replace: true })
-   }
+      if (roledRoutes) {
+         if (roledRoutes.children) {
+            roledRoutes = roledRoutes.children.find(child => child.to === route.path)
+         }
+         const userRole = useAuthStore().getRole
+         if (roledRoutes?.roles !== '*' && !roledRoutes?.roles?.includes(userRole)) return navigateTo('/error/403')
+      }
+   } else return navigateTo('/error/404', { replace: true })
 })
