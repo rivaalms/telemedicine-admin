bc2de49969b5688a015f2f2846d14665d3719659
Riva Almero <riva@dev.netkromsolution.com>
2023-10-26 12:03:48 +0700

    docs(middleware): code documentations

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,10 +1,14 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
    const authStore = useAuthStore()
+   // Define routes that doesn't need authentication validation
    const excludedRoutes = [
       '/login',
       '/forgot-password'
    ]
 
+   // If the user visits any route except excluded ones AND the user hasn't logged in, redirects user to login page
    if (!excludedRoutes.includes(to.path) && !authStore.isLoggedIn) return navigateTo('/login')
+
+   // If the user visits any of excluded routes AND the user has logged in, redirects user to home page
    else if (excludedRoutes.includes(to.path) && authStore.isLoggedIn) return navigateTo('/', { replace: true })
 })
--- middleware/routes.global.ts
+++ middleware/routes.global.ts
@@ -1,12 +1,18 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+   // Find the route inside Nuxt Router. If route not found, return 404
    const route = useRouter().getRoutes().find(item => item.name === to.name)
    if (route) {
+      // Find the route's roles validation inside the sidebar routes
       let roledRoutes = useRoutes.find(item => item.to === route.path || item.children?.find(child => child.to === route.path))
 
+      // If the route exist, continue with roles validation. Else continue to the route (i.e. the route does not have roles validation)
       if (roledRoutes) {
+         // If the route is a child route, extract the route from its parent
          if (roledRoutes.children) {
             roledRoutes = roledRoutes.children.find(child => child.to === route.path)
          }
+
+         // Get the user's role, then check if the role exist in route's roles. If the role doen't exist, return 403
          const userRole = useAuthStore().getRole
          if (roledRoutes?.roles !== '*' && !roledRoutes?.roles?.includes(userRole)) return navigateTo('/error/403')
       }
